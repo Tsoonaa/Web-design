0ba247f911241e16466d1392531293410ec09025
armanakond <147554657+armanakond@users.noreply.github.com>
2024-03-28 19:32:56 +0000

    splash.js

--- splash.js
+++ splash.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () { 
     setTimeout(function () {
       document.querySelector(".splash").style.display = "none";
-      window.location.href = "shop.html"; //put the home page here.
+      window.location.href = "home.html";
     }, 4000); //waits for 4000ms which = 4s
   });
