47f5ad3078ae6e86a98ea73b0d9828cbcd87badf
armanakond <147554657+armanakond@users.noreply.github.com>
2024-03-31 08:58:44 +0100

    Delete splash.js

--- splash.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () { 
-    setTimeout(function () {
-      document.querySelector(".splash").style.display = "none";
-      window.location.href = "home.html";
-    }, 4000); //waits for 4000ms which = 4s
-  });
